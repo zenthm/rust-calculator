b7dc8f184ca11c48066cee51fae685aff2645d8c
Zenthm <zenthm.dev@gmail.com>
2024-02-28 11:09:37 +0800

    Add user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
     println!("Calculator in Rust");
+
+    loop {
+        println!("Input: ");
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+    }
 }
